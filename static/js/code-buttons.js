76cc0845b09bfc7edb1fe25ab44120165363057f
Naomi Alterman <naomila@uw.edu>
2024-03-18 16:38:59 -0700

    Begin working on visible whitespace toggle

--- static/js/code-buttons.js
+++ static/js/code-buttons.js
@@ -1,7 +1,6 @@
 function codeCopy(id) {
     const block = document.getElementById(id);
     const button = block.getElementsByTagName("button")[0];
-    console.log(button)
     const all_code = block.getElementsByTagName("code");
     const code = all_code[all_code.length-1];
     let copy_lines = [];
@@ -33,6 +32,37 @@ function codeExpand(id) {
     const block = document.getElementById(id);
     const button = block.getElementsByTagName("button")[1];
     block.classList.toggle("code-expanded")
+    button.blur();
+
+}
+
+
+function codeWsToggle(id) {
+    const block = document.getElementById(id);
+    const button = block.getElementsByTagName("button")[1];
+
+    if (!block.classList.contains("code-ws-annotated")) {
+        const all_code = block.getElementsByTagName("code");
+        const code = all_code[all_code.length-1];
+        for (let line of code.querySelectorAll(':scope > span')) {
+            for (let region of line.getElementsByTagName('span')) {
+                for (let frag of region.childNodes) {
+                    if (frag.nodeType === Node.TEXT_NODE) {
+                        
+                        while ((wsidx = frag.textContent.search(/\s/)) > -1) {
+                            frag = frag.splitText(wsidx);
+                            frag = frag.splitText(1);
+                        }
+                        console.log(frag)
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    block.classList.toggle("code-ws-visible")
     button.blur();
 
 }
